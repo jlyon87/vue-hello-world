8ab12b2ef2719b682f45eee079e2b12eaa8d3ef4
jlyon87 <jml6487@gmail.com>
2017-12-08 22:12:49 -0500

    exercise 3 complete

---
+++ Exercise 3/app.js
@@ -0,0 +1,21 @@
+new Vue({
+	el: '#exercise',
+	data: {
+		value: 0
+	},
+
+	computed: {
+		result: function() {
+			return this.value >= 37 ? "Done" : "not there yet";
+		}
+	},
+
+	watch: {
+		result: function(value) {
+			var vm = this;
+			setTimeout(function() {
+				vm.value = 0;
+			}, 5000);
+		}
+	}
+});
