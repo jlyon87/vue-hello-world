0f2e5ba238091452492b270172a202966c4295ff
jlyon87 <jml6487@gmail.com>
2017-12-11 23:32:21 -0500

    S9 Dynamic Components

---
+++ S9 - Advanced Components/Project/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<button @click="selectedComponent = 'app-quote'">Quote</button>
+				<button @click="selectedComponent = 'app-author'">Author</button>
+				<button @click="selectedComponent = 'app-new'">New</button>
+				{{ selectedComponent }}
+				<hr>
+				<keep-alive>
+					<component :is="selectedComponent">
+						default content
+					</component>
+				</keep-alive>
+				<!-- <app-quote >
+					<h2 slot="title" >{{ quoteTitle }}</h2>
+					<p >A wonderful Quote</p>
+				</app-quote> -->
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
+
+export default {
+	components: {
+		"app-quote": Quote,
+		"app-author": Author,
+		"app-new": New
+	},
+	data() {
+		return {
+			quoteTitle: "The Quote",
+			selectedComponent: "app-quote"
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ S9 - Advanced Components/Project/src/components/New.vue
@@ -0,0 +1,26 @@
+<template >
+	<div >
+		<h3>New Quote</h3>
+		<button @click="counter++">Counter</button>
+		<p>{{ counter }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			counter: 0
+		}
+	},
+	destroyed() {
+		console.log("New destroyed");
+	},
+	deactivated() {
+		console.log("Leaving New");
+	},
+	activated() {
+		console.log("Arriving on New");
+	}
+}
+</script>
