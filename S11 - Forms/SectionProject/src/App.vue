23029c50be07ef958afdd05b61d819322be2458f
jlyonas <jlyon@acumensolutions.com>
2017-12-12 13:29:07 -0500

    add custom form control switch component

--- S11 - Forms/SectionProject/src/App.vue
+++ S11 - Forms/SectionProject/src/App.vue
@@ -1,104 +1,112 @@
 <template>
-	<div class="container">
-		<form>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<h1>File a Complaint</h1>
-					<hr>
-					<div class="form-group">
-						<label for="email">Mail</label>
-						<input
-						type="text"
-						id="email"
-						class="form-control"
-						v-model.lazy="user.email" >
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input
-						type="password"
-						id="password"
-						class="form-control"
-						v-model.lazy="user.password">
-					</div>
-					<div class="form-group">
-						<label for="age">Age</label>
-						<input
-						type="number"
-						id="age"
-						class="form-control"
-						v-model.lazy="user.age">
-					</div>
+<div class="container">
+<form>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+			<h1>File a Complaint</h1>
+			<hr>
+			<div class="form-group">
+				<label for="email">Mail</label>
+				<input
+				type="text"
+				id="email"
+				class="form-control"
+				:value="user.email"
+				@change="user.email = $event.target.value" >
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input
+				type="password"
+				id="password"
+				class="form-control"
+				v-model.lazy="user.password">
+			</div>
+			<div class="form-group">
+				<label for="age">Age</label>
+				<input
+				type="number"
+				id="age"
+				class="form-control"
+				v-model.lazy="user.age">
+			</div>
 
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-					<label for="message">Message</label><br>
-					<!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-					<textarea
-					id="message"
-					rows="5"
-					class="form-control"
-					v-model.lazy="message" ></textarea>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<div class="form-group">
-						<label for="sendmail">
-							<input
-							type="checkbox"
-							id="sendmail"
-							value="SendMail"
-							v-model="mailBoxes"> Send Mail
-						</label>
-						<label for="sendInfomail">
-							<input
-							type="checkbox"
-							id="sendInfomail"
-							value="SendInfoMail"
-							v-model="mailBoxes"> Send Infomail
-						</label>
-					</div>
-
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-					<label for="male">
-						<input
-						type="radio"
-						id="male"
-						value="Male"
-						v-model="gender"> Male
-					</label>
-					<label for="female">
-						<input
-						type="radio"
-						id="female"
-						value="Female"
-						v-model="gender"> Female
-					</label>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-					<label for="priority">Priority</label>
-					<select
-					id="priority"
-					class="form-control"
-					v-model="priority">
-					<option v-for="opt in priorityOptions"
-						:value="opt.value" :selected="opt.value === 'Low'" >{{ opt.label }}</option>
-				</select>
-			</div>
 		</div>
-		<hr>
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<button
-				class="btn btn-primary">Submit!
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+			<label for="message">Message</label><br>
+			<!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+			<textarea
+			id="message"
+			rows="5"
+			class="form-control"
+			v-model.lazy="message" ></textarea>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+			<div class="form-group">
+				<label for="sendmail">
+					<input
+					type="checkbox"
+					id="sendmail"
+					value="SendMail"
+					v-model="mailBoxes"> Send Mail
+				</label>
+				<label for="sendInfomail">
+					<input
+					type="checkbox"
+					id="sendInfomail"
+					value="SendInfoMail"
+					v-model="mailBoxes"> Send Infomail
+				</label>
+			</div>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+			<label for="male">
+				<input
+				type="radio"
+				id="male"
+				value="Male"
+				v-model="gender"> Male
+			</label>
+			<label for="female">
+				<input
+				type="radio"
+				id="female"
+				value="Female"
+				v-model="gender"> Female
+			</label>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+			<label for="priority">Priority</label>
+			<select
+				id="priority"
+				class="form-control"
+				v-model="priority">
+
+				<option v-for="opt in priorityOptions"
+					:value="opt.value" :selected="opt.value === 'Low'" >{{ opt.label }}</option>
+				</select>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+			<app-switch v-model="dataSwitch"></app-switch>
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+			<button
+				class="btn btn-primary"
+				@click.prevent="">Submit!
 			</button>
 		</div>
 	</div>
@@ -121,7 +129,7 @@
 				</ul>
 				<p>Gender: {{ gender }}</p>
 				<p>Priority: {{ priority }}</p>
-				<p>Switched:</p>
+				<p>Switched: {{ dataSwitch }}</p>
 			</div>
 		</div>
 	</div>
@@ -130,6 +138,8 @@
 </template>
 
 <script>
+import Switch from "./Switch.vue";
+
 export default {
 	data() {
 		return {
@@ -146,8 +156,13 @@ export default {
 				{label: "High", value: "High"},
 				{label: "Medium", value: "Medium"},
 				{label: "Low", value: "Low"}
-			]
+			],
+			dataSwitch: true
 		}
+	},
+
+	components: {
+		"app-switch": Switch
 	}
 }
 </script>
---
+++ S11 - Forms/SectionProject/src/Switch.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+	<div>
+		<div :class="{active: value}"
+			@click="switched(true)"
+			id="on" >On</div>
+		<div :class="{active: !value}"
+			@click="switched(false)"
+			id="off" >Off</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+	props: ["value"],
+
+	methods: {
+		switched(isOn) {
+			this.$emit("input", isOn);
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+#on, #off {
+	width: 40px;
+	height: 20px;
+	background-color: lightgray;
+	padding: 2px;
+	display: inline-block;
+	margin: 10px -2px;
+	box-sizing: content-box;
+	cursor: pointer;
+	text-align: center;
+}
+
+#on:hover, #on.active {
+	background-color: lightgreen;
+}
+
+#off:hover, #off.active {
+	background-color: lightcoral;
+}
+</style>
