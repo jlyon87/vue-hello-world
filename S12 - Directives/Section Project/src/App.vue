8f36c2cf576a797e03ea06cc6211cb56c24bfe3b
jlyonas <jlyon@acumensolutions.com>
2017-12-15 08:44:38 -0500

    Global Directive Complete

--- S12 - Directives/Section Project/src/App.vue
+++ S12 - Directives/Section Project/src/App.vue
@@ -2,8 +2,16 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Directives</h1>
+				<h1>Built in Directives</h1>
+				<p v-text="'Stuff'"></p>
+				<p v-html="'<strong>Strong Text</strong>'"></p>
+			</div>
+		</div>
 
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1>Custom Directives</h1>
+				<p v-highlight:background.delayed="'red'">Color This</p>
 			</div>
 		</div>
 	</div>
@@ -11,9 +19,10 @@
 
 <script>
 export default {
+	directives: {
+		'local-highlight': {
+			bind() {}
+		}
+	}
 }
 </script>
-
-<style>
-
-</style>
--- S12 - Directives/Section Project/src/main.js
+++ S12 - Directives/Section Project/src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Global Directive
+Vue.directive('highlight', {
+	bind(el, binding, vnode) {
+		let delay = 0;
+		if (binding.modifiers["delayed"]) { // modifiers
+			delay = 3000;
+		}
+
+		setTimeout(() => {
+			if (binding.arg === "background") { // arguments
+				el.style.backgroundColor = binding.value; // value
+			} else {
+				el.style.color = binding.value;
+			}
+		}, delay);
+	}
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
