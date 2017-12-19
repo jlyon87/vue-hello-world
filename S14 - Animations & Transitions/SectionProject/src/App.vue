587ce0fd475f17f194aae2237290d01c52498a27
jlyon87 <jml6487@gmail.com>
2017-12-19 10:05:06 -0500

    begin S14 mini app

--- S14 - Animations & Transitions/SectionProject/src/App.vue
+++ S14 - Animations & Transitions/SectionProject/src/App.vue
@@ -11,6 +11,7 @@
 				<br>
 				<button class="btn btn-primary" @click="show = !show" >Show Alert</button>
 				<br><br>
+
 				<transition :name="alertAnimation">
 					<div class="alert alert-info" v-if="show">This is some info.</div>
 				</transition>
@@ -29,18 +30,122 @@
 					leave-active-class="animated shake" >
 					<div class="alert alert-info" v-if="show">This is some info.</div>
 				</transition>
+
+				<transition :name="alertAnimation" mode="out-in">
+					<div class="alert alert-info" v-if="show" key="info" >This is some info.</div>
+					<div class="alert alert-warning" v-else key="warning" >This is some info.</div>
+				</transition>
+
+				<hr>
+
+				<button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+				<br><br>
+				<transition
+					:css="false"
+					@before-enter="beforeEnter"
+					@enter="enter"
+					@after-enter="afterEnter"
+					@enter-cancelled="enterCancelled"
+
+					@before-leave="beforeLeave"
+					@leave="leave"
+					@after-leave="afterLeave"
+					@leave-cancelled="leaveCancelled">
+
+					<div
+						v-if="load"
+						style="width: 100px; height: 100px; background-color: lightgreen;"></div>
+				</transition>
+				<hr>
+				<button class="btn btn-primary" @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert': selectedComponent = 'app-success-alert'">Component Toggle</button>
+				<br><br>
+				<transition name="fade" mode="out-in">
+					<component :is="selectedComponent"></component>
+				</transition>
+
+				<hr>
+
+				<button class="btn btn-primary" @click="addItem">Add</button>
+				<br><br>
+				<transition-group name="slide" tag="ul" class="list-group">
+					<li
+						class="list-group-item"
+						v-for="(number, i) in numbers"
+						@click="removeItem(i)"
+						style="cursor:pointer"
+						:key="number">
+
+						{{ number }}
+					</li>
+				</transition-group>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import DangerAlert from "./DangerAlert.vue";
+import SuccessAlert from "./SuccessAlert.vue";
+
 export default {
 	data() {
 		return {
-			show: true,
-			alertAnimation: "fade"
+			show: false,
+			load: true,
+			alertAnimation: "fade",
+			selectedComponent: 'app-success-alert',
+			numbers: [1, 2, 3, 4, 5]
 		}
+	},
+
+	methods: {
+		addItem() {
+			const pos = Math.floor(Math.random() * this.numbers.length);
+			this.numbers.splice(pos, 0, this.numbers.length + 1);
+		},
+
+		removeItem(index) {
+			this.numbers.splice(index, 1);
+		},
+
+		beforeEnter(el) {
+			console.log("beforeEnter", el);
+		},
+
+		enter(el, done) {
+			console.log("Enter");
+			done();
+		},
+
+		afterEnter(el) {
+			console.log("afterEnter");
+		},
+
+		enterCancelled(el) {
+			console.log("enterCancelled");
+		},
+
+		beforeLeave(el) {
+			console.log("beforeLeave", el);
+		},
+
+		leave(el, done) {
+			console.log("leave");
+			done();
+		},
+
+		afterLeave(el) {
+			console.log("afterLeave");
+		},
+
+		leaveCancelled(el) {
+			console.log("leaveCancelled");
+		}
+	},
+
+	components: {
+		"app-danger-alert": DangerAlert,
+		"app-success-alert": SuccessAlert
 	}
 }
 </script>
@@ -80,8 +185,13 @@ export default {
 
 	.slide-leave-active {
 		animation: slide-out 1s ease-out forwards;
-		transition: opacity 3s;
+		transition: opacity 1s;
 		opacity: 0;
+		position: absolute;
+	}
+
+	.slide-move {
+		transition: transform 1s;
 	}
 
 	@keyframes slide-in {
