408e0c8af45dcb6106ed9b8a1485590cac0967ce
jlyon87 <jml6487@gmail.com>
2017-12-23 09:42:47 -0500

    add routes

---
+++ S18 - Stock Trader/Project/src/routes.js
@@ -0,0 +1,27 @@
+import Home from "./components/Home.vue";
+import Portfolio from "./components/portfolio/Portfolio.vue";
+import Stocks from "./components/stocks/Stocks.vue";
+
+export const routes = [
+  {
+    path: "",
+    name: "home",
+    components: {
+      default: Home,
+    }
+  },
+  {
+    path: "/portfolio",
+    name: "portfolio",
+    components: {
+      default: Portfolio,
+    }
+  },
+  {
+    path: "/Stocks",
+    name: "stocks",
+    components: {
+      default: Stocks
+    }
+  }
+];
