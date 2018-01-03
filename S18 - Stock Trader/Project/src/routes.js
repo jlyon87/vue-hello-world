0b2497681ae0d1f8e9ba16baba0ad9e2364d571b
jlyonas <jlyon@acumensolutions.com>
2018-01-03 14:28:30 -0500

    fix direct urls

--- S18 - Stock Trader/Project/src/routes.js
+++ S18 - Stock Trader/Project/src/routes.js
@@ -3,25 +3,29 @@ import Portfolio from "./components/portfolio/Portfolio.vue";
 import Stocks from "./components/stocks/Stocks.vue";
 
 export const routes = [
-  {
-    path: "",
-    name: "home",
-    components: {
-      default: Home,
-    }
-  },
-  {
-    path: "/portfolio",
-    name: "portfolio",
-    components: {
-      default: Portfolio,
-    }
-  },
-  {
-    path: "/Stocks",
-    name: "stocks",
-    components: {
-      default: Stocks
-    }
-  }
+	{
+		path: "/",
+		name: "home",
+		components: {
+			default: Home,
+		}
+	},
+	{
+		path: "/portfolio",
+		name: "portfolio",
+		components: {
+			default: Portfolio,
+		}
+	},
+	{
+		path: "/Stocks",
+		name: "stocks",
+		components: {
+			default: Stocks
+		}
+	},
+	{
+		path: "*",
+		redirect: "/"
+	}
 ];
